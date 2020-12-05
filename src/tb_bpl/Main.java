6f949eb56665fb83b8abcfa872d362c4b2b9fdb3
intanyuliana <72247967+intanyuliana@users.noreply.github.com>
2020-12-05 08:29:50 +0700

    first

---
+++ src/tb_bpl/Main.java
@@ -0,0 +1,10 @@
+package tb_bpl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tb_bpl/mysqlConnection.java
@@ -0,0 +1,20 @@
+package tb_bpl;
+
+import java.sql.*;
+import javax.swing.*;
+
+public class mysqlConnection {
+	Connection conn = null;
+	public static Connection dbConnector() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/tb_bpl?serverTimezone=Asia/Jakarta","root","");
+			return conn;
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+			return null;
+		}
+	}
+	
+}
