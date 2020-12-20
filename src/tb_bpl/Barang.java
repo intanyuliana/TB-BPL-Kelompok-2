9504d5ef6f090baf72d5b41c7d12d61756bd997c
Immalatunil <73459961+Immalatunil@users.noreply.github.com>
2020-12-20 14:09:46 +0700

    Update Barang.java

--- src/tb_bpl/Barang.java
+++ src/tb_bpl/Barang.java
@@ -30,5 +30,26 @@ public class Barang {
 		System.out.print("Jumlah Penambahan stok\t\t: ");
 		this.restok = input.nextInt();
 	}
+
+	public void edit() {
+		System.out.print("Kode Barang yang akan di Update\t : ");
+		this.sku = input.next();
+		System.out.print("Kode Barang\t: ");
+		this.sku = input.next();
+		System.out.print("Nama Barang\t: ");
+		this.nama = input.next();
+		System.out.print("Jumlah\t\t: ");
+		this.stok = input.nextInt();
+		System.out.print("Harga Beli\t: Rp ");
+		this.hargabeli = input.nextInt();
+		System.out.print("Harga Jual\t: Rp ");
+		this.hargajual = input.nextInt();
+		
+	}
+
+	public void delete() {
+		System.out.print("Kode Barang yang akan di Delete\t : ");
+		this.sku = input.next();
+	}
 	
 }
